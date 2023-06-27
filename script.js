a925f70404af710dcc4158038aafe1ad7df9ba18
dkrishna11 <116519809+dkrishna11@users.noreply.github.com>
2023-06-27 15:36:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,30 +1,52 @@
-//your code here!
-// Get the list element
-const list = document.getElementById('infi-list');
+// Wait for the document to finish loading
+document.addEventListener("DOMContentLoaded", function() {
+  // Get the list element
+  var list = document.getElementById("infi-list");
 
-// Function to check if the user has reached the end of the list
-function isEndOfList() {
-  const listRect = list.getBoundingClientRect();
-  return listRect.bottom <= window.innerHeight;
-}
+  // Set the number of items to be added when reaching the end of the list
+  var itemsToAdd = 2;
 
-// Function to add more list items to the list
-function addMoreItems() {
-  // Add 2 more list items
-  for (let i = 0; i < 2; i++) {
-    const listItem = document.createElement('li');
-    listItem.textContent = 'List Item';
-    list.appendChild(listItem);
+  // Function to check if the user has reached the end of the list
+  function isEndOfList() {
+    var lastItem = list.lastElementChild;
+    var lastItemOffset = lastItem.offsetTop + lastItem.clientHeight;
+    var pageOffset = window.pageYOffset + window.innerHeight;
+    return pageOffset > lastItemOffset;
   }
-}
 
-// Event listener for scrolling
-window.addEventListener('scroll', () => {
-  if (isEndOfList()) {
-    addMoreItems();
+  // Function to add more list items
+  function addMoreItems() {
+    for (var i = 0; i < itemsToAdd; i++) {
+      var listItem = document.createElement("li");
+      listItem.textContent = "List Item";
+      list.appendChild(listItem);
+    }
   }
+
+  // Event listener for scrolling
+  window.addEventListener("scroll", function() {
+    if (isEndOfList()) {
+      addMoreItems();
+    }
+  });
+
+  // Intersection Observer for scrolling
+  var options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 1.0
+  };
+
+  var observer = new IntersectionObserver(function(entries) {
+    if (entries[0].isIntersecting) {
+      addMoreItems();
+    }
+  }, options);
+
+  // Observe the last item
+  var lastItem = list.lastElementChild;
+  observer.observe(lastItem);
+
+  // Add initial list items
+  addMoreItems();
 });
-
-// Initial load
-addMoreItems();
-
